f1b16896dab1efbb07d3e5a057a81543b5237113
abhi.Logics <abhi.kumar252004@gmail.com>
2025-06-07 02:18:56 +0530

    al

--- src/components/code/code.js
+++ src/components/code/code.js
@@ -2,7 +2,7 @@
 
 import React, { useRef, useState, useEffect } from 'react';
 import Highlight, { defaultProps } from 'prism-react-renderer';
-import theme from 'prism-react-renderer/themes/nightOwl'; // ✅ use available theme
+import theme from 'prism-react-renderer/themes/nightOwl';// ✅ use available theme
 
 import { res, esp, ard } from '../../../code';
 
