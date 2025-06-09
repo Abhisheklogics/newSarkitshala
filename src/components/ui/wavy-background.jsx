751b4ebbe2681a812776772dcbccb786a2c69cdd
Abhishek Kumar <166478718+Abhisheklogics@users.noreply.github.com>
2025-06-09 19:59:03 +0530

    new wavy-background.jsx

--- src/components/ui/wavy-background.jsx
+++ src/components/ui/wavy-background.jsx
@@ -100,7 +100,7 @@ export const WavyBackground = ({
     <div
       className={cn("h-[300px]  md:h-screen   flex flex-col items-center justify-center", containerClassName)}>
       <canvas
-        className="absolute inset-0 z-0 md:mt-0 mt-[-290px]"
+        className="absolute inset-0 z-0 md:mt-0 mt-[-300px]"
         ref={canvasRef}
         id="canvas"
         style={{
